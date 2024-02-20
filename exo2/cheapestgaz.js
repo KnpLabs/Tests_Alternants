a2ab1d8cb7162e6e2ac936fad3253dda988cbb21
LeoGrouet <leo.grouet@gmail.com>
2024-02-20 13:53:28 +0100

    exo2: rework of first fetch to get only one item of each region

--- exo2/cheapestgaz.js
+++ exo2/cheapestgaz.js
@@ -1,21 +1,19 @@
-// First , I will get every info for each region based on the region code
-
 async function cheapestGaz() {
   const response = await fetch(
-    `https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?select=region&limit=100`
+    `https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?select=region&group_by=region`
   );
   const data = await response.json();
   const regions = data.results;
 
   const regionName = [];
 
-  // I loop through the data to get the region code
+  // I loop through the data to get all the region_code
   regions.forEach((region) => {
-    // If the code already exist in the array, I don't push it
-    if (regionName.includes(region.region)) {
+    // If the region_code already exist in the array, I don't push it
+    if (regionName.includes(region.region) || region.region === null) {
       return;
     }
-    // If the code doesn't exist in the array, I push it
+    // If the region_code doesn't exist in the array, I push it
     regionName.push(region.region);
     return regionName.sort();
   });
@@ -25,41 +23,32 @@ async function cheapestGaz() {
     const sp98 = "sp98_prix";
     const gazole = "gazole_prix";
 
-    const priceResponseForsp95 = await fetch(
+    // The filter is done with the URL. Each URL return the element where the price for each fuel is the lowest
+    const apiResponseForsp95 = await fetch(
       `https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?select=adresse%2C%20ville%20%2C%20cp%20%2C%20sp95_prix%2C%20sp98_prix%20%2C%20gazole_prix&where=region%20%3D%20%22${item}%22&order_by=${sp95}&limit=1`
     );
 
-    const priceResponseForsp98 = await fetch(
+    const apiResponseForsp98 = await fetch(
       `https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?select=adresse%2C%20ville%20%2C%20cp%20%2C%20sp95_prix%2C%20sp98_prix%20%2C%20gazole_prix&where=region%20%3D%20%22${item}%22&order_by=${sp98}&limit=1`
     );
 
-    const priceResponseForgazole = await fetch(
+    const apiResponseForgazole = await fetch(
       `https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?select=adresse%2C%20ville%20%2C%20cp%20%2C%20sp95_prix%2C%20sp98_prix%20%2C%20gazole_prix&where=region%20%3D%20%22${item}%22&order_by=${gazole}&limit=1`
     );
 
-    const priceDatasp95 = await priceResponseForsp95.json();
-    const priceDatasp98 = await priceResponseForsp98.json();
-    const priceDatagazole = await priceResponseForgazole.json();
+    const datasp95 = await apiResponseForsp95.json();
+    const datasp98 = await apiResponseForsp98.json();
+    const datagazole = await apiResponseForgazole.json();
 
-    const finalDataSP95 = priceDatasp95.results[0];
-    const finalDataSP98 = priceDatasp98.results[0];
-    const finalDataGazole = priceDatagazole.results[0];
-
-    const erreur = "Erreur : Pas de données pour cette région.";
+    const finalDataSP95 = datasp95.results[0];
+    const finalDataSP98 = datasp98.results[0];
+    const finalDataGazole = datagazole.results[0];
 
     const logText = `
       ${item} :
-      SP95    : ${finalDataSP95.sp95_prix} € / ${finalDataSP95.adresse} ${
-      finalDataSP95.cp
-    } ${finalDataSP95.ville}
-      SP98    : ${(finalDataSP98.sp98_prix = null
-        ? erreur
-        : finalDataSP98.sp98_prix)} € / ${finalDataSP98.adresse} ${
-      finalDataSP98.cp
-    } ${finalDataSP98.ville}
-      Gazole  : ${finalDataGazole.gazole_prix} € / ${finalDataGazole.adresse} ${
-      finalDataGazole.cp
-    } ${finalDataGazole.ville}
+      SP95    : ${finalDataSP95.sp95_prix} € / ${finalDataSP95.adresse} ${finalDataSP95.cp} ${finalDataSP95.ville}
+      SP98    : ${finalDataSP98.sp98_prix} € / ${finalDataSP98.adresse} ${finalDataSP98.cp} ${finalDataSP98.ville}
+      Gazole  : ${finalDataGazole.gazole_prix} € / ${finalDataGazole.adresse} ${finalDataGazole.cp} ${finalDataGazole.ville}
       `;
 
     console.log(logText);
