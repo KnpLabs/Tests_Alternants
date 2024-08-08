41f737b3b4d290c59984580af740abb0093fb613
Thomas Plantais <thomasplantais@gmail.com>
2024-08-08 21:38:04 +0200

    Add get_cheapest_price() by region and fuel function

--- exo2/main.py
+++ exo2/main.py
@@ -9,3 +9,14 @@ def get_regions() -> list:
     for result in response.json()["results"]:
         regions.append(result["region"])
     return regions
+
+def get_cheapest_price(region: str, fuel: str) -> tuple:
+    url = f"{BASE_URL}?select={fuel}_prix%2C%20adresse%2C%20cp%2C%20ville&where=region%20%3D%20%22{region}%22%20and%20{fuel}_prix%20is%20not%20null&order_by={fuel}_prix&limit=1"
+    response = requests.get(url)
+    if response.json()["total_count"] > 0:
+        result = response.json()["results"][0]
+        price = result[f"{fuel}_prix"]
+        address = f"{result['adresse']} {result['cp']} {result['ville']}"
+        return (price, address)
+    else:
+        return (None, None)
